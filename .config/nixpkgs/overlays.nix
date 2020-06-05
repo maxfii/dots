a79ef6a4dc26590495a67f7b31f30d0a1a95cc56
Max Fiialkovskyi <maxfiialkovskyi@protonmail.com>
2020-06-05 19:23:30 +0300

    install packages throught nix overlay

---
+++ .config/nixpkgs/overlays.nix
@@ -0,0 +1,22 @@
+let overlay1 = self: super: with super;
+{
+    myprofile =
+        [
+            yq
+            jq
+            awk
+            htop
+            neovim
+            discord
+            ghcid
+            ghc
+            cabal-install
+            stylish-haskell
+            ripgrep
+            tdesktop
+            tmux
+            tree
+        ];
+};
+
+in [ overlay1 ]
